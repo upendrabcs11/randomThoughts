1692981af73628b56631daa1cae5ea5adad6b935
pooja <pujakumari2805@gmail.com>
2017-08-16 23:28:26 +0530

    insertionsort in c

---
+++ Sorting/c/insertion.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#define MAX 8
+void insertionsort(int[] );
+void display(int[]);
+int  main()
+{
+	int a[MAX];
+	int k;
+	printf("enter value in array");
+	for(k=0;k<MAX;k++)
+	scanf("%d",&a[k]);
+	insertionsort(a);
+	display(a);
+	return 0;
+}
+void insertionsort(int a[])
+{
+	int k;
+	for(k=1;k<MAX;k++)
+	{ int T,p;
+	T=a[k];
+	p=k-1;
+	while(T<a[p]&&p>=0)
+	{
+		a[p+1]=a[p];
+		p=p-1;
+	}
+	a[p+1]=T;
+}	
+	
+}
+void display(int a[])
+{
+	int k;
+	printf("display sorted list");
+	for(k=0;k<MAX;k++)
+	printf("%d\n",a[k]);
+}
+
+
